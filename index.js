f72814758a8593f6c33b13528458994939582561
NaziaHladun <fabulous7351@gmail.com>
2023-11-10 19:22:51 +0200

    Connected DB, added the display of countries

--- index.js
+++ index.js
@@ -1,14 +1,41 @@
 import express from "express";
 import bodyParser from "body-parser";
+import pg from "pg";
+
+const db = new pg.Client({
+  user: "postgres",
+  host: "localhost",
+  database: "world",
+  password: "admin",
+  port: 5432,
+});
 
 const app = express();
 const port = 3000;
 
+db.connect();
+
+//Take from DB countries code
+let countries = [];
+db.query("SELECT country_code FROM visited_countries", (err, res) => {
+  if(err){
+    console.error("Error executing query", err.stack)
+  } else {
+    res.rows.forEach((country) => {
+      countries.push(country.country_code);
+    });
+  }
+  db.end();
+});
+
+//Middleware
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+//Get home page
 app.get("/", async (req, res) => {
-  //Write your code here.
+  console.log(countries);
+  res.render("index.ejs", {countries: countries, total: countries.length});
 });
 
 app.listen(port, () => {
