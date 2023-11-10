19c6bcc55fc4d9977ca3b1112ae828ab2364ad7d
NaziaHladun <fabulous7351@gmail.com>
2023-11-11 01:47:03 +0200

    Now we can insert new countries

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import bodyParser from "body-parser";
+import capitalize from "capitalize";
 import pg from "pg";
 
 const db = new pg.Client({
@@ -15,27 +16,38 @@ const port = 3000;
 
 db.connect();
 
-//Take from DB countries code
-let countries = [];
-db.query("SELECT country_code FROM visited_countries", (err, res) => {
-  if(err){
-    console.error("Error executing query", err.stack)
-  } else {
-    res.rows.forEach((country) => {
-      countries.push(country.country_code);
-    });
-  }
-  db.end();
-});
-
 //Middleware
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+async function checkVisisted() {
+  const result = await db.query("SELECT country_code FROM visited_countries");
+  const countries = [];
+  result.rows.forEach((country) => {
+      countries.push(country.country_code);
+  });
+  return countries;
+}
+
 //Get home page
 app.get("/", async (req, res) => {
+  const countries = await checkVisisted();
   console.log(countries);
   res.render("index.ejs", {countries: countries, total: countries.length});
+});
+
+//INSERT new country
+app.post("/add", async (req, res) => {
+  const request = capitalize(req.body.country.toLowerCase().trim());
+  const result = await db.query("SELECT country_code FROM countries  WHERE country_name = $1", [request]);
+  
+  if(result.rows.length !== 0){
+    const data = result.rows[0];
+    const new_country_code = data.country_code;
+    await db.query("INSERT INTO visited_countries (country_code) VALUES ($1)", [new_country_code]);
+  }
+
+  res.redirect("/");
 });
 
 app.listen(port, () => {
